ea52a4cfe18706d3e308366031bf25dd60adae81
Sam Moreira <mor13009@byui.edu>
2019-09-24 17:50:28 -0600

    Messing around with my assignments page.

--- web/assigns/assign.php
+++ web/assigns/assign.php
@@ -24,7 +24,7 @@
         <h2> Below you will find the indiviual assignments done throughout the semester.</h2>
         
         <div class="btn-list">
-            <button type="button" onclick="window.location.href = 'https://young-escarpment-18200.herokuapp.com/hello.html';"
+            <button type="button" onclick="window.location.href = 'https://young-escarpment-18200.herokuapp.com/w1hello.html';"
                 class="btn btn-dark">W1 Hello World</button>
             <button type="button" onclick="window.location.href = 'https://young-escarpment-18200.herokuapp.com/...';"
                 class="btn btn-dark disabled">W2 ---</button>
@@ -53,35 +53,6 @@
             <button type="button" onclick="window.location.href = 'https://young-escarpment-18200.herokuapp.com/pproject/index.php';"
                 class="btn btn-dark disabled">Final Project</button>
         </div>
-    </div>
-
-    <div class="list-group">
-        <a onclick="window.location.href = 'https://young-escarpment-18200.herokuapp.com/w1hello.html';"
-        class="list-group-item list-group-item-action list-group-item-dark">W1 Hello World</a>
-        <a onclick="window.location.href = 'https://young-escarpment-18200.herokuapp.com/assign.php';"
-        class="list-group-item list-group-item-action list-group-item-dark disabled">Week 2 ---</a>
-        <a onclick="window.location.href = 'https://young-escarpment-18200.herokuapp.com/assign.php';"
-        class="list-group-item list-group-item-action list-group-item-dark diasbled">Week 3 ---</a>
-        <a onclick="window.location.href = 'https://young-escarpment-18200.herokuapp.com/assign.php';"
-        class="list-group-item list-group-item-action list-group-item-dark disabled">Week 4 ---</a>
-        <a onclick="window.location.href = 'https://young-escarpment-18200.herokuapp.com/assign.php';"
-        class="list-group-item list-group-item-action list-group-item-dark diasbled">Week 5 ---</a>
-        <a onclick="window.location.href = 'https://young-escarpment-18200.herokuapp.com/assign.php';"
-        class="list-group-item list-group-item-action list-group-item-dark disabled">Week 6 ---</a>
-        <a onclick="window.location.href = 'https://young-escarpment-18200.herokuapp.com/assign.php';"
-        class="list-group-item list-group-item-action list-group-item-dark diasbled">Week 7 ---</a>
-        <a onclick="window.location.href = 'https://young-escarpment-18200.herokuapp.com/assign.php';"
-        class="list-group-item list-group-item-action list-group-item-dark disabled">Week 8 ---</a>
-        <a onclick="window.location.href = 'https://young-escarpment-18200.herokuapp.com/assign.php';"
-        class="list-group-item list-group-item-action list-group-item-dark diasbled">Week 9 ---</a>
-        <a onclick="window.location.href = 'https://young-escarpment-18200.herokuapp.com/assign.php';"
-        class="list-group-item list-group-item-action list-group-item-dark disabled">Week 10 ---</a>
-        <a onclick="window.location.href = 'https://young-escarpment-18200.herokuapp.com/assign.php';"
-        class="list-group-item list-group-item-action list-group-item-dark diasbled">Week 11 ---</a>
-        <a onclick="window.location.href = 'https://young-escarpment-18200.herokuapp.com/assign.php';"
-        class="list-group-item list-group-item-action list-group-item-dark disabled">Week 12 ---</a>
-        <a onclick="window.location.href = 'https://young-escarpment-18200.herokuapp.com/assign.php';"
-        class="list-group-item list-group-item-action list-group-item-dark diasbled">Week 13 ---</a>
     </div>
 
     <?php
