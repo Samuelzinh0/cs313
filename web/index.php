f7e5a829de20fbfc3f3f9bd9ace1cb0858fd6cf2
Sam Moreira <mor13009@byui.edu>
2019-09-27 09:51:22 -0600

    Finilised my assignment by adding some images to some pages.

--- web/index.php
+++ web/index.php
@@ -29,7 +29,7 @@
     echo "Today's date is " . date("d-m-Y") . "<br>";
     ?>
 
-    <img src="images/sam1.jpg" style="img" alt="Website Creator">
+    <img src="images/sam1.jpg" class="thumbnail" alt="Website Creator">
     
     </body>
 </html>
