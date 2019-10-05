cfee090a21631b18c336501aa3f61e24099680c6
Sam Moreira <mor13009@byui.edu>
2019-10-04 20:01:25 -0600

    Testing my shopping cart application.

--- web/assigns/week03/index.php
+++ web/assigns/week03/index.php
@@ -1,67 +1,58 @@
 <?php
 session_start();
 ?>
-
 <!DOCTYPE html>
-<html lang="en">
-
-<head>
-<title>Week 03 Shopping Cart</title>
-<link href="stylist.css" type="text/css" rel="stylesheet" />
+<html lang="en"><head>
+<TITLE>PHP Shopping Cart without Database</TITLE>
+<link href="style.css" type="text/css" rel="stylesheet" />
 </head>
-
 <body>
-<h1>This is the Shopping Cart Application using PHP</h1>
-
-<?php
-require_once "products.php";
+<h1>Demo Shopping Cart without Database</h1>
+<?php 
+require_once "product-gallery.php";
 ?>
-
 <div class="clear-float"></div>
-<div id="shopping-cart"></div> 
-<div class"text-heading">Shopping Cart<a id="btnEmpty" class="cart-action" onClick="cartAction('empty','');"><img src="images/icon-empty.png" />Empty Cart</a></div>
+<div id="shopping-cart">
+<div class="txt-heading">Shopping Cart <a id="btnEmpty" class="cart-action" onClick="cartAction('empty','');"><img src="images/icon-empty.png" /> Empty Cart</a></div>
 <div id="cart-item">
-
-<?php
+<?php 
 require_once "ajax-action.php";
 ?>
-
 </div>
 </div>
-
 <script src="jquery-3.2.1.min.js" type="text/javascript"></script>
 <script>
 function cartAction(action, product_code) {
     var queryString = "";
-    if(action != "") {
+    if (action != "") {
         switch (action) {
-            case "add":
-                queryString = 'action=' + action + '&code=' + product_code + '&quantity=' + $("#qty_" + product_code).val();
-                break;
-            case "remove":
-                queryString = 'action=' + action + '&code=' + product_code;
-                break;
-            case "empty":
-                queryString = 'action=' + action;
-                break;
+        case "add":
+            queryString = 'action=' + action + '&code=' + product_code
+                    + '&quantity=' + $("#qty_" + product_code).val();
+            break;
+        case "remove":
+            queryString = 'action=' + action + '&code=' + product_code;
+            break;
+        case "empty":
+            queryString = 'action=' + action;
+            break;
         }
     }
-
     jQuery.ajax({
         url : "ajax-action.php",
         data : queryString,
         type : "POST",
         success : function(data) {
             $("#cart-item").html(data);
-            if (action == "add") { 
-                $("#add_" + product_code + " img").attr("src", "images/icon-check.png");
+            if (action == "add") {
+                $("#add_" + product_code + " img").attr("src",
+                        "images/icon-check.png");
                 $("#add_" + product_code).attr("onclick", "");
             }
         },
         error : function() {
         }
     });
-
 }
 </script>
 </body>
