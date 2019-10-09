13462543c9c761be25052679bfcd7cd86c463312
Sam Moreira <mor13009@byui.edu>
2019-10-09 15:53:00 -0600

    Just adding the sql document we did as a group with the commands for adding tables ot the database.

---
+++ web/assigns/team/ta04.sql
@@ -0,0 +1,8 @@
+
+CREATE TABLE "user" ( "id" int );
+CREATE TABLE "notes" ( "noteId" int, "speaker_id" varchar, "conference_id" varchar, "id" int );
+CREATE TABLE "speakers" ( "speaker_id" int );
+CREATE TABLE "conference" ( "year" int, "month" int, "conference_id" varchar );
+ALTER TABLE "notes" ADD FOREIGN KEY ("speaker_id") REFERENCES "speakers" ("speaker_id");
+ALTER TABLE "notes" ADD FOREIGN KEY ("conference_id") REFERENCES "conference" ("conference_id");
+ALTER TABLE "notes" ADD FOREIGN KEY ("id") REFERENCES "user" ("id");
