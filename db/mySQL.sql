97cd24d2c509c13d818f6ef5fc02b4af08d0e95b
Sam Moreira <mor13009@byui.edu>
2019-10-15 16:00:10 -0600

    pushing ta activity.

--- db/mySQL.sql
+++ db/mySQL.sql
@@ -1,15 +1,6 @@
-CREATE TABLE "users" (
-  "id" SERIAL PRIMARY KEY,
-  "user_name" varchar,
-  "user_type" varchar
-);
-
-CREATE TABLE "comments" (
-  "id" SERIAL PRIMARY KEY,
-  "comment_title" varchar,
-  "created_at" varchar,
-  "rating" int,
-  "users_id" int
-);
+CREATE TABLE "users" ("id" SERIAL PRIMARY KEY, "user_name" varchar, "user_type" varchar);
+CREATE TABLE "comments" ("id" SERIAL PRIMARY KEY, "comment_title" varchar, "created_at" varchar, "users_id" int);
+CREATE TABLE "ratings" ("id" SERIAL PRIMARY KEY, "ranting" int, "users_id" int);
 
 ALTER TABLE "comments" ADD FOREIGN KEY ("users_id") REFERENCES "users" ("id");
+ALTER TABLE "ratings" ADD FOREIGN KEY ("users_id") REFERENCES "users" ("id");
