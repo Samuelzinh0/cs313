53811495bd62ee533d49755eab93bda772a6195c
Sam Moreira <mor13009@byui.edu>
2019-10-15 17:27:01 -0600

    testinggg

--- web/assigns/week05/week05.php
+++ web/assigns/week05/week05.php
@@ -18,22 +18,19 @@
 <h1>test</h1>
 <?php
   $temp_comment_title;
-  $temp_comment_text;
-  $temp_users_id;
 
   foreach ($comment_infos as $comment_info) {
     $id = $comment_info['users_id'];
     global $temp_comment_title, $temp_comment_text, $temp_users_id;
     $temp_comment_title = $comment_info['comment_title'];
     $temp_comment_text = $comment_info['comment_text'];
+    echo "<br><br>";
+    echo "<h3>Reviews</h3>";
+    echo "<p><pre>$temp_comment_title</pre><p>";
+    echo "<br>";
+    echo "<p><pre> Comment: $temp_comment_text</pre></p>";
+    echo "<br>";
   }
-
-  echo "<br><br>";
-  echo "<h3>Reviews</h3>";
-  echo "<p><pre>$temp_comment_title</pre><p>";
-  echo "<br>";
-  echo "<p><pre> Comment: $temp_comment_text</pre></p>";
-  echo "<br>";
 ?>
 
 </body>
