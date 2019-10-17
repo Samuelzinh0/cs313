96eeb3eadf1e83968a8935f81b1ed193def5a4fb
Sam Moreira <mor13009@byui.edu>
2019-10-17 15:11:50 -0600

    Adding a link to my week 05 assignment from ym main website.

--- web/assigns/week05/week05.php
+++ web/assigns/week05/week05.php
@@ -11,7 +11,7 @@
 <html lang="eng">
 
 <head>
-  <title>Bootstrap Example</title>
+  <title>Database Info</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
