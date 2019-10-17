0f634390b4c9f8fb762ef2ed3e759e9a2a815257
Sam Moreira <mor13009@byui.edu>
2019-10-17 14:44:56 -0600

    Adding a link to my week 05 assignment from ym main website.

--- web/assigns/assign.php
+++ web/assigns/assign.php
@@ -73,8 +73,8 @@
             <button type="button" onclick="window.location.href = 'https://young-escarpment-18200.herokuapp.com/...';"
                 class="btn btn-dark disabled">W4 ---</button>
             <button type="button" onclick="window.location.href = 'https://young-escarpment-18200.herokuapp.com/...';"
-                class="btn btn-dark disabled">W5 ---</button>
-            <button type="button" onclick="window.location.href = 'https://young-escarpment-18200.herokuapp.com/...';"
+                class="btn btn-dark disabled">W5 </button>
+            <button type="button" onclick="window.location.href = 'https://young-escarpment-18200.herokuapp.com/assigns/week05/week05.php';"
                 class="btn btn-dark disabled">W6 ---</button>
             <button type="button" onclick="window.location.href = 'https://young-escarpment-18200.herokuapp.com/...';"
                 class="btn btn-dark disabled">W7 ---</button>
