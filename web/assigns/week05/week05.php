19952759813564c62346ea0a73b612c5eb2140ec
Sam Moreira <mor13009@byui.edu>
2019-10-17 14:57:47 -0600

    Adding a link to my week 05 assignment from ym main website.

--- web/assigns/week05/week05.php
+++ web/assigns/week05/week05.php
@@ -12,11 +12,22 @@
 
 <head>
 
+<h1>Pulling Information from ym Database</h1>
+<div class="jumbotron text-center">
+  <h1>My First Bootstrap Page</h1>
+  <p>Resize this responsive page to see the effect!</p>
+</div>
 </head>
 
 <body>
-<h1>test</h1>
-<?php
+
+<div class="container">
+  <div class="row">
+    <div class="col-sm-4">
+      <h3></h3>
+      <p>Reviews</p>
+      <?php
+  echo "<h3>Reviews</h3>";
   $temp_comment_title;
 
   foreach ($comment_infos as $comment_info) {
@@ -25,12 +36,33 @@
     $temp_comment_title = $comment_info['comment_title'];
     $temp_comment_text = $comment_info['comment_text'];
     echo "<br><br>";
-    echo "<h3>Reviews</h3>";
     echo "<p><pre>$temp_comment_title</pre><p>";
     echo "<br>";
     echo "<p><pre> Comment: $temp_comment_text</pre></p>";
     echo "<br>";
   }
+?>
+
+    </div>
+  </div>
+</div>
+<?php
+/*
+  echo "<h3>Reviews</h3>";
+  $temp_comment_title;
+
+  foreach ($comment_infos as $comment_info) {
+    $id = $comment_info['users_id'];
+    global $temp_comment_title, $temp_comment_text, $temp_users_id;
+    $temp_comment_title = $comment_info['comment_title'];
+    $temp_comment_text = $comment_info['comment_text'];
+    echo "<br><br>";
+    echo "<p><pre>$temp_comment_title</pre><p>";
+    echo "<br>";
+    echo "<p><pre> Comment: $temp_comment_text</pre></p>";
+    echo "<br>";
+  }
+*/
 ?>
 
 </body>
