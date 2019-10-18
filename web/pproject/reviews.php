0cce6350a652e667a6ddd6996d8fda41ccd9f630
Sam Moreira <mor13009@byui.edu>
2019-10-18 14:08:08 -0600

    Adding ym database skills to my personal project.

--- web/pproject/reviews.php
+++ web/pproject/reviews.php
@@ -1,34 +1,57 @@
+<?php
+  require('dbConnect.php');
+  $db = get_db();
+  $query = 'SELECT comment_title, comment_text, users_id FROM comments;';
+  /*$query .= 'SELECT rating, users_id FROM ratings;';
+  $query .= 'SELECT user_name, users_id FROM users;';*/
+  $stmt = $db->prepare($query);
+  $stmt->execute();
+  $comment_infos = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  //vardump(comment_infos);
+
+?>
+
 <!DOCTYPE html>
+<html lang="eng">
 
-<html>
-    <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-    <link rel="stylesheet" href="stylist.css">
-    </head>
-    <title>Accent Curbing: Reviews</title>
-    <body>
-    <div class="text-center">
-    <h1> Reviews </h1>
-    <h3> This is where you can see what other customers thought of our services.</h3>
-        <div class="btn-group">
-            <button type="button" onclick="window.location.href = 'https://young-escarpment-18200.herokuapp.com/pproject/index.php';"
-              class="btn btn-dark">Home Page</button>
-            <button type="button" onclick="window.location.href = 'https://young-escarpment-18200.herokuapp.com/pproject/pastjobs.php';"
-              class="btn btn-light">Past Jobs</button>
-            <button type="button" onclick="window.location.href = 'https://young-escarpment-18200.herokuapp.com/pproject/pricing.php';"
-              class="btn btn-light">Pricing</button>
-            <button type="button" onclick="window.location.href = 'https://young-escarpment-18200.herokuapp.com/pproject/reviews.php';"
-              class="btn btn-light">Reviews</button>
-        </div>
-        <p> Feel free to leave a review of the services you have received from us!</p>
-        
+<head>
+  <title>Database Info</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+
+  <div class="jumbotron text-center">
+    <h1>Pulling Information from my Database</h1>
+  </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-4">
+        <h3></h3>
+        <p>Reviews</p>
+        <?php
+        $temp_comment_title;
+        /*$temp_comment_rating;*/
+        foreach ($comment_infos as $comment_info) {
+          $id = $comment_info['users_id'];
+          global $temp_comment_title, $temp_comment_text, $temp_users_id;
+          $temp_comment_title = $comment_info['comment_title'];
+          $temp_comment_text = $comment_info['comment_text'];
+          /*$temp_comment_rating = $comment_info['rating'];*/
+          echo "<br><br>";
+          echo "<p><pre>$temp_comment_title</pre><p>";
+          echo "<br>";
+          echo "<p><pre> Comment: $temp_comment_text</pre></p>";
+          echo "<br>";
+          }
+        ?>
+      </div>
     </div>
+  </div>
 
-    <?php
-    echo "Today's date is " . date("d-m-Y") . "<br>";
-    ?>
-    
-    </body>
+</body>
 </html>
