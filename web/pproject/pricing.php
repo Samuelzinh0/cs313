28cb609786c7d89031ed67876814d2dfefa6fb1d
Sam Moreira <mor13009@byui.edu>
2019-10-18 15:16:39 -0600

    Working on my personal project.

--- web/pproject/pricing.php
+++ web/pproject/pricing.php
@@ -16,6 +16,19 @@
   </div>
     <div class="text-center">
     <h3> We will showcase the different moulding we use.</h3>
+    <nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">WebSiteName</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Home</a></li>
+      <li><a href="#">Page 1</a></li>
+      <li><a href="#">Page 2</a></li>
+      <li><a href="#">Page 3</a></li>
+    </ul>
+  </div>
+</nav>
         <div class="btn-group">
             <button type="button" onclick="window.location.href = 'https://young-escarpment-18200.herokuapp.com/pproject/index.php';"
               class="btn btn-dark">Home Page</button>
