8609970984d83f50cd45b4a754abf9b5025c698c
Sam Moreira <mor13009@byui.edu>
2019-10-22 16:19:13 -0600

    messing around with the reviews page.

--- web/pproject/reviews.php
+++ web/pproject/reviews.php
@@ -21,7 +21,7 @@
 <html lang="eng">
 
 <head>
-  <title>Database Info</title>
+  <title>Customer Reviews</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
@@ -61,13 +61,13 @@
         <p>Reviews</p>
         <?php
         $temp_comment_title;
-        /*$temp_comment_rating;*/
+        $temp_comment_rating;
         foreach ($comment_infos as $comment_info) {
           $id = $comment_info['users_id'];
           global $temp_comment_title, $temp_comment_text, $temp_users_id;
           $temp_comment_title = $comment_info['comment_title'];
           $temp_comment_text = $comment_info['comment_text'];
-          /*$temp_comment_rating = $comment_info['rating'];*/
+          $temp_comment_rating = $comment_info['rating'];
           echo "<br><br>";
           echo "<p><pre>$temp_comment_title</pre><p>";
           echo "<br>";
