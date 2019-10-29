8a71b43db8dcfe8877bd5e491b388cd717463c98
Sam Moreira <mor13009@byui.edu>
2019-10-29 14:58:14 -0600

    making the forms look pretty.

--- web/pproject/reviews.php
+++ web/pproject/reviews.php
@@ -55,24 +55,22 @@
   */
 ?>
 
-<form>
+<form action = "insertInfo">
+  <div class="form-group">
+    <label for="exampleFormControlInput1">User name:</label>
+    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+  </div>
   <div class="form-group">
     <label for="exampleFormControlInput1">Email address</label>
     <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
   </div>
   <div class="form-group">
-    <label for="exampleFormControlSelect1">Example select</label>
-    <select class="form-control" id="exampleFormControlSelect1">
-      <option>1</option>
-      <option>2</option>
-      <option>3</option>
-      <option>4</option>
-      <option>5</option>
-    </select>
+    <label for="exampleFormControlInput1">Email address</label>
+    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
   </div>
   <div class="form-group">
-    <label for="exampleFormControlSelect2">Example multiple select</label>
-    <select multiple class="form-control" id="exampleFormControlSelect2">
+    <label for="reviewRating">Review Rating:</label>
+    <select class="form-control" id="reviewRating">
       <option>1</option>
       <option>2</option>
       <option>3</option>
