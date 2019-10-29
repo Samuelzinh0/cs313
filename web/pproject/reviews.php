a1ffefd1e3d23bb7a00cefdc1e66186c92c71174
Sam Moreira <mor13009@byui.edu>
2019-10-29 15:06:07 -0600

    making the forms look pretty.

--- web/pproject/reviews.php
+++ web/pproject/reviews.php
@@ -55,18 +55,14 @@
   */
 ?>
 
-<form action = "insertInfo">
+<form action="insertInfo" method="get">
   <div class="form-group">
-    <label for="exampleFormControlInput1">User name:</label>
-    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+    <label for="nameOfUser">User name:</label>
+    <input type="text" class="form-control" id="nameOfUser" placeholder="(Your name here)">
   </div>
   <div class="form-group">
-    <label for="exampleFormControlInput1">Email address</label>
-    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
-  </div>
-  <div class="form-group">
-    <label for="exampleFormControlInput1">Email address</label>
-    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+    <label for="exampleFormControlInput1">Review Title:</label>
+    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="(Your review title here)">
   </div>
   <div class="form-group">
     <label for="reviewRating">Review Rating:</label>
@@ -79,8 +75,15 @@
     </select>
   </div>
   <div class="form-group">
-    <label for="exampleFormControlTextarea1">Example textarea</label>
-    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+    <label for="userType">Type of User:</label>
+    <select class="form-control" id="userType">
+      <option>Landscape Designer</option>
+      <option>Homeowner</option>
+    </select>
+  </div>
+  <div class="form-group">
+    <label for="commentContent">Review Content:</label>
+    <textarea class="form-control" id="commentContent" rows="3"></textarea>
   </div>
 </form>
 
