169adf724104caa493670577fbd2db9b021914b8
Sam Moreira <mor13009@byui.edu>
2019-10-29 15:59:53 -0600

    making the forms look pretty.

--- web/pproject/reviews.php
+++ web/pproject/reviews.php
@@ -102,11 +102,12 @@
           $temp_comment_rating = $comment_info['rating'];
           $temp_comment_person = $comment_info['user_name'];
 
-          echo "<br><br>";
-          echo '<p style="border:2px solid grey">'.$temp_comment_title.'Rating: '.$temp_comment_rating.'/5</p>';
+          
+          echo '<br><div style="border:2px solid grey>';
+          echo "<p> $temp_comment_title Rating: $temp_comment_rating/5</p>";
           echo "<br>";
           echo "<p> $temp_comment_person: $temp_comment_text</p>";
-          echo "<br>";
+          echo "<br></div>";
           }
         ?>
       </div>
